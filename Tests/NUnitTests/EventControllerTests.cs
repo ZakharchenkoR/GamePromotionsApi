dacedacbe03c909d088c772817bbafd9b9a6333d
Roman Zakharchenko <rzakharchenko@vector-software.com>
2023-06-14 17:02:14 +0200

    Add Tests

---
+++ Tests/NUnitTests/EventControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using GamePromotion.API.Common;
+using GamePromotion.API.Controllers;
+using GamePromotion.BAL.Enums;
+using GamePromotion.BAL.Models;
+using GamePromotion.BAL.Models.Abstractions;
+using GamePromotion.BAL.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    public class EventControllerTests
+    {
+        private Mock<IEventService> _mockEventService;
+        private Mock<IBroadcaster> _mockBroadcaster;
+        private EventController _eventController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockEventService = new Mock<IEventService>();
+            _mockBroadcaster = new Mock<IBroadcaster>();
+
+            _eventController = new EventController(_mockEventService.Object, _mockBroadcaster.Object);
+        }
+
+        [Test]
+        public async Task Get_ReturnsOkWithEvents()
+        {
+            // Arrange
+            var events = GetEventModels();
+            _mockEventService.Setup(service => service.GetEvents()).ReturnsAsync(events);
+
+            // Act
+            var result = await _eventController.Get();
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(events, okResult.Value);
+        }
+
+        [Test]
+        public async Task Post_ReturnsOkWithAddedEvent()
+        {
+            // Arrange
+            var eventModel = new AddEventModel() 
+            { 
+                Name = "Test",
+                StartsAt = DateTime.Now,
+                ExpiresAt = DateTime.Now,
+                EventType = EventTypes.Tournament
+            };
+
+            var returnedEvent = new EventModel() 
+            {
+                Id = 1,
+                Name = "Test",
+                StartsAt =  DateTime.Now,
+                ExpiresAt= DateTime.Now,
+                EventType = EventTypes.Tournament
+            };
+
+            _mockEventService.Setup(service => service.AddEvent(It.IsAny<AddEventModel>())).ReturnsAsync(returnedEvent);
+
+            // Act
+            var result = await _eventController.Post(eventModel);
+
+            // Assert
+            _mockEventService.Verify(service => service.AddEvent(It.Is<AddEventModel>(e => e == eventModel)), Times.Once());
+            _mockBroadcaster.Verify(service => service.BroadcastPromotion(It.IsAny<IPromotion>()), Times.Once());
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(returnedEvent, okResult.Value);
+        }
+
+        private List<EventModel> GetEventModels()
+        {
+            return new List<EventModel> {
+                new EventModel
+                {
+                    Id = 1,
+                    Name = "Test",
+                    StartsAt = DateTime.Now,
+                    ExpiresAt = DateTime.Now,
+                    EventType = EventTypes.Tournament
+                },
+                new EventModel
+                {
+                    Id = 1,
+                    Name = "Test",
+                    StartsAt = DateTime.Now,
+                    ExpiresAt = DateTime.Now,
+                    EventType = EventTypes.Competition
+                }
+            };
+        }
+    }
+}
---
+++ Tests/NUnitTests/OfferControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using GamePromotion.API.Common;
+using GamePromotion.API.Controllers;
+using GamePromotion.BAL.Enums;
+using GamePromotion.BAL.Models;
+using GamePromotion.BAL.Models.Abstractions;
+using GamePromotion.BAL.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    internal class OfferControllerTests
+    {
+        private Mock<IOfferService> _mockOfferService;
+        private Mock<IBroadcaster> _mockBroadcaster;
+        private OfferController _offerController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockOfferService = new Mock<IOfferService>();
+            _mockBroadcaster = new Mock<IBroadcaster>();
+
+            _offerController = new OfferController(_mockOfferService.Object, _mockBroadcaster.Object);
+        }
+
+        [Test]
+        public async Task Get_ReturnsOkWithOffers()
+        {
+            // Arrange
+            var offers = GetOffersModels();
+            _mockOfferService.Setup(service => service.GetOffers()).ReturnsAsync(offers);
+
+            // Act
+            var result = await _offerController.Get();
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(offers, okResult.Value);
+        }
+
+        [Test]
+        public async Task Post_ReturnsOkWithAddedOffers()
+        {
+            // Arrange
+            var offerModel = new AddOfferModel()
+            {
+                Name = "Test",
+                StartsAt = DateTime.Now,
+                ExpiresAt = DateTime.Now,
+                OfferType = OfferTypes.GameCurrencyPack
+            };
+
+            var returnedOffer = new OfferModel()
+            {
+                Id = 1,
+                Name = "Test",
+                StartsAt = DateTime.Now,
+                ExpiresAt = DateTime.Now,
+                OfferType = OfferTypes.GameCurrencyPack
+            };
+
+            _mockOfferService.Setup(service => service.AddOffer(It.IsAny<AddOfferModel>())).ReturnsAsync(returnedOffer);
+
+            // Act
+            var result = await _offerController.Post(offerModel);
+
+            // Assert
+            _mockOfferService.Verify(service => service.AddOffer(It.Is<AddOfferModel>(e => e == offerModel)), Times.Once());
+            _mockBroadcaster.Verify(service => service.BroadcastPromotion(It.IsAny<IPromotion>()), Times.Once());
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(returnedOffer, okResult.Value);
+        }
+
+        private List<OfferModel> GetOffersModels()
+        {
+            return new List<OfferModel> {
+                new OfferModel
+                {
+                    Id = 1,
+                    Name = "Test",
+                    StartsAt = DateTime.Now,
+                    ExpiresAt = DateTime.Now,
+                    OfferType = OfferTypes.GameCurrencyPack
+                },
+                new OfferModel
+                {
+                    Id = 1,
+                    Name = "Test",
+                    StartsAt = DateTime.Now,
+                    ExpiresAt = DateTime.Now,
+                    OfferType = OfferTypes.DiscountedItemPurchase
+                }
+            };
+        }
+    }
+}
